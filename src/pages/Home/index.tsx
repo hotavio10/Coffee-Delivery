1f354e199723a830bd542311dc1c0e7ad5860ee9
hotavio10 <hotavio10@gmail.com>
2024-04-07 22:36:32 -0300

    feat: add update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,9 @@
-import { Container}
-
-
+import { Container } from "./style"
 export function Home() {
   
   return(
     <Container >
-      Encontre o café perfeito para qualquer hora do dia
+      Encontre o café perfeito para qualquer hora do dia.
     </Container>
   )
 }
