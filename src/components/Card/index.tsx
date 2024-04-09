603849100cfc225af0d3bd9bb6847a633aefd738
hotavio10 <hotavio10@gmail.com>
2024-04-09 19:45:04 -0300

    feat: add card update

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,24 +1,39 @@
+import { CoffeeImg, Description, Price, Tags, Title } from "./styles";
+
 type CardProps = {
-coffee: {
-  id: string,
-  title: string,
-  description: string,
-  tags: string[], 
-  price: number,
-  image: string,
-}
+  coffee: {
+    id: string,
+    title: string,
+    description: string,
+    tags: string[],
+    price: number,
+    image: string,
+  }
 }
 
 export function Card({ coffee }: CardProps) {
   return (
     <section>
+      <CoffeeImg>
       <img src={coffee.image} alt={coffee.title} />
+      </CoffeeImg>
       <div>
-        {coffee.tags.map(tag => <p>{tag}</p>)}
-        
-        <h2>{coffee.title}</h2>
-        <p>{coffee.description}</p>
-        <p> R${coffee.price.toFixed(2)}</p>
+
+        <Tags>
+          {coffee.tags.map(tag => <p>{tag}</p>)}
+        </Tags>
+
+        <Title>
+          <h2>{coffee.title}</h2>
+        </Title>
+
+        <Description>
+          <p>{coffee.description}</p>
+        </Description>
+
+        <Price>
+          <p> R${coffee.price.toFixed(2)}</p>
+        </Price>
       </div>
     </section>
   );
